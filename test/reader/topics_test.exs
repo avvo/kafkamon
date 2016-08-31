5cb733b4fb3a5c198da5a1ab196bb17b2ece0978
Donald Plummer <dplummer@avvo.com>
2016-08-31 10:31:31 -0400

    Test Reader.Topics

--- test/reader/topics_test.exs
+++ test/reader/topics_test.exs
@@ -1,5 +1,27 @@
 defmodule Reader.TopicsTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
-  
+  setup do
+    KafkaMockStore.set_topics(["foo"])
+    Reader.Topics.fetch_topics
+    :ok
+  end
+
+  test ".new_subscriber casts the requester the topics" do
+    Reader.Topics.new_subscriber()
+    assert_receive {:"$gen_cast", {:topics, [], ["foo"]}}
+  end
+
+  test ".fetch_topics updates the state with the new topics" do
+    assert Reader.Topics.current_topics() == ["foo"]
+    KafkaMockStore.set_topics(["foo", "moo"])
+    Reader.Topics.fetch_topics
+    assert Reader.Topics.current_topics() == ["foo", "moo"]
+  end
+
+  test ".current_topics returns the current topics in alphabetical order" do
+    KafkaMockStore.set_topics(["foo", "bar"])
+    assert Reader.Topics.current_topics() == ["bar", "foo"]
+  end
+
 end
--- test/support/kafka_mock.ex
+++ test/support/kafka_mock.ex
@@ -1,9 +1,30 @@
+defmodule KafkaMockStore do
+  def start_link, do: Agent.start_link(fn -> %{} end, name: __MODULE__)
+
+  def topics do
+    Agent.get(__MODULE__, fn state ->
+      state |> Map.get(:topics, [])
+    end)
+  end
+
+  def set_topics(new_topics) do
+    Agent.update(__MODULE__, fn state ->
+      state |> Map.put(:topics, new_topics)
+    end)
+  end
+
+end
+
 defimpl Kafka, for: Mock do
+  def start_link do
+    KafkaMockStore.start_link
+  end
+
   def metadata(_opts \\ []) do
+    start_link()
     %{
-      topic_metadatas: [
-        %{topic: "foo"}
-      ]
+      topic_metadatas: KafkaMockStore.topics
+        |> Enum.map(&(%{topic: &1}))
     }
   end
 
@@ -26,4 +47,5 @@ defimpl Kafka, for: Mock do
       }
     ]
   end
+
 end
