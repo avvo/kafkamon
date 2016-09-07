183a6bc06a5ae37c5d165105a8718538a119f0c6
Donald Plummer <dplummer@avvo.com>
2016-09-06 21:36:27 -0700

    Fix test to match recently changed interface

--- test/reader/event_queue/foreman_test.exs
+++ test/reader/event_queue/foreman_test.exs
@@ -14,28 +14,28 @@ defmodule Reader.EventQueue.ForemanTest do
   test "starts a new child worker when a topic is added", %{foreman: foreman, eqs: eqs} do
     assert Supervisor.count_children(eqs).workers == 0
     assert capture_log([level: :info, format: "$message", colors: [enabled: false]], fn ->
-      send foreman, {:topics, [], ["foo"]}
+      send foreman, {:topics, ["foo"]}
       assert Foreman.known_topics(foreman) == ["foo"]
     end) == "" # no logs, like already started
     assert Supervisor.count_children(eqs).workers == 1
   end
 
   test "terminates a worker when a topic is removed", %{foreman: foreman, eqs: eqs} do
-    send foreman, {:topics, [], ["foo"]}
+    send foreman, {:topics, ["foo"]}
     assert Foreman.known_topics(foreman) == ["foo"]
     assert Supervisor.count_children(eqs).workers == 1
 
-    send foreman, {:topics, ["foo"], []}
+    send foreman, {:topics, []}
     assert Foreman.known_topics(foreman) == []
     assert Supervisor.count_children(eqs).workers == 0
   end
 
   test "doesn't blow up when creating an already existing worker", %{foreman: foreman, eqs: eqs} do
-    send foreman, {:topics, [], ["foo"]}
+    send foreman, {:topics, ["foo"]}
     assert Foreman.known_topics(foreman) == ["foo"]
 
     {:ok, foreman2} = Foreman.start_link(supervisor: eqs)
-    send foreman2, {:topics, [], ["foo"]}
+    send foreman2, {:topics, ["foo"]}
     assert Foreman.known_topics(foreman2) == ["foo"]
 
     assert Supervisor.count_children(eqs).workers == 1
