8b96e6220916d7d9ff1b2f2d0aefe2b55ec72bb1
Donald Plummer <dplummer@avvo.com>
2016-09-15 16:15:00 -0700

    Build and publish with gitsha instead of latest

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 APP_NAME=kafkamon
-VERSION=${1:-latest}
+VERSION=${1:-`git rev-parse --short HEAD`}
 
 docker build --rm -t $APP_NAME:$VERSION .
--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 APP_NAME=kafkamon
-VERSION=${1:-latest}
+VERSION=${1:-`git rev-parse --short HEAD`}
 
 docker tag $APP_NAME:$VERSION dplummer1avvo/$APP_NAME:$VERSION
 docker push dplummer1avvo/$APP_NAME:$VERSION
