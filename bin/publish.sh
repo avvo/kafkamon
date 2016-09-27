a542a9b498bd4f84e4e8571e2de6f3b904e7833a
Donald Plummer <dplummer@avvo.com>
2016-09-27 16:11:14 -0700

    Tag as latest too

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 APP_NAME=kafkamon
-VERSION=${1:-`git rev-parse --short HEAD`}
+VERSION=${1:-latest}
+GITSHA=`git rev-parse --short HEAD`
+
+docker tag $APP_NAME:$GITSHA dplummer1avvo/$APP_NAME:$GITSHA
+docker push dplummer1avvo/$APP_NAME:$GITSHA
 
 docker tag $APP_NAME:$VERSION dplummer1avvo/$APP_NAME:$VERSION
 docker push dplummer1avvo/$APP_NAME:$VERSION
