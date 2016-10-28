60d7b672b52779f36accc8dd1582fafbfc1dd5da
Donald Plummer <dplummer@avvo.com>
2016-10-28 12:20:47 -0700

    Use different topic names to prevent collision

--- test/reader/event_queue/foreman_test.exs
+++ test/reader/event_queue/foreman_test.exs
@@ -16,15 +16,15 @@ defmodule Reader.EventQueue.ForemanTest do
     capture_log(fn -> 1 end) # clear the log
     assert Supervisor.count_children(eqs).workers == 0
     assert capture_log([level: :info, format: "$message", colors: [enabled: false]], fn ->
-      send foreman, {:topics, [{"foo", 3}]}
-      assert Foreman.known_topics(foreman) == [{"foo", 3}]
+      send foreman, {:topics, [{"foo1", 3}]}
+      assert Foreman.known_topics(foreman) == [{"foo1", 3}]
     end) == "" # no logs, like already started
     assert Supervisor.count_children(eqs).workers == 3
   end
 
   test "terminates a worker when a topic is removed", %{foreman: foreman, eqs: eqs} do
-    send foreman, {:topics, [{"foo", 3}]}
-    assert Foreman.known_topics(foreman) == [{"foo", 3}]
+    send foreman, {:topics, [{"foo2", 3}]}
+    assert Foreman.known_topics(foreman) == [{"foo2", 3}]
     assert Supervisor.count_children(eqs).workers == 3
 
     send foreman, {:topics, []}
@@ -33,12 +33,12 @@ defmodule Reader.EventQueue.ForemanTest do
   end
 
   test "doesn't blow up when creating an already existing worker", %{foreman: foreman, eqs: eqs} do
-    send foreman, {:topics, [{"foo", 3}]}
-    assert Foreman.known_topics(foreman) == [{"foo", 3}]
+    send foreman, {:topics, [{"foo3", 3}]}
+    assert Foreman.known_topics(foreman) == [{"foo3", 3}]
 
     {:ok, foreman2} = Foreman.start_link(supervisor: eqs, topic_subscribe: false)
-    send foreman2, {:topics, [{"foo", 3}]}
-    assert Foreman.known_topics(foreman2) == [{"foo", 3}]
+    send foreman2, {:topics, [{"foo3", 3}]}
+    assert Foreman.known_topics(foreman2) == [{"foo3", 3}]
 
     assert Supervisor.count_children(eqs).workers == 3
   end
