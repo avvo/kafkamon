c82333b39893346dfac59624d5d8c4750d1ddd11
Donald Plummer <dplummer@avvo.com>
2016-11-11 11:44:13 -0800

    Tone down how many events the test producer writes

--- lib/mix/tasks/kafkamon.test_producer.ex
+++ lib/mix/tasks/kafkamon.test_producer.ex
@@ -1,32 +1,37 @@
 defmodule Mix.Tasks.Kafkamon.TestProducer do
   use Mix.Task
 
+  @event_type 'AvvoEvent.AvvoProAdded'
 
-  @shortdoc "Write as many events as fast as we can to Kafka"
+  @shortdoc "Write some events to kafka for testing"
 
   def run(_args) do
-    KafkaEx.start nil, nil
+    {:ok, brokers} = KafkaImpl.Util.kafka_brokers()
+    {:ok, worker} = KafkaImpl.create_no_name_worker(brokers, :no_consumer_group)
 
     schema_path = "test/data/AvvoProAdded.avsc"
     {:ok, schema_json} = File.read(schema_path)
-    type = 'AvvoEvent.AvvoProAdded'
-    v = %{event: %{app_id: "a", name: "n", timestamp: 0}, lawyer_id: 0}
-    message = Avrolixr.Codec.encode!(v, schema_json, type)
 
     ProgressBar.render_spinner [
-      text: "Producing to 'test'",
+      text: "Producing to 'users'",
       spinner_color: IO.ANSI.magenta,
       interval: 100,
       frames: :braille
     ], fn ->
-      produce("test", message, 50_000)
+      produce("users", 50, worker, schema_json)
     end
   end
 
-  def produce(_topic, _message, 0), do: nil
+  def produce(_topic, 0, _worker, _), do: nil
 
-  def produce(topic, message, iterations_left) do
-    KafkaEx.produce topic, 0, message
-    produce(topic, message, iterations_left - 1)
+
+  def produce(topic, iterations_left, worker, schema_json) do
+    ts = DateTime.utc_now |> DateTime.to_unix()
+    v = %{event: %{app_id: "a", name: "n", timestamp: ts}, lawyer_id: 100000 + iterations_left}
+    message = Avrolixr.Codec.encode!(v, schema_json, @event_type)
+    partition = rem(iterations_left, 12)
+    KafkaEx.produce topic, partition, message, worker_name: worker
+    :timer.sleep 100
+    produce(topic, iterations_left - 1, worker, schema_json)
   end
 end
