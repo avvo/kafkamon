b45e935509744c06e10a418e02a9a41c0e57cfea
Victor <github.viclim@gmail.com>
2020-12-16 14:20:18 +0800

    Use session_id as consumer group id

--- ruby/kafkamon.rb
+++ ruby/kafkamon.rb
@@ -4,6 +4,8 @@ AvvoKafka.configure do |c|
 end
 
 class Kafkamon < Sinatra::Base
+  enable :sessions
+
   set :server, :puma
   set :streams, []
 
@@ -13,10 +15,11 @@ class Kafkamon < Sinatra::Base
 
   get '/stream' do
     content_type 'text/event-stream'
+    session_id = session['session_id']
     stream :keep_open do |out|
       settings.streams << out
       thread = Thread.new do
-        AvvoKafka.consume(topic: 'directory-attorney-availability', group_id: 'kafkamon-rb') do |message|
+        AvvoKafka.consume(topic: 'directory-attorney-availability', group_id: "kafkamon-rb-#{session_id}") do |message|
           out << "event: message\n\n"
           out << "data: #{JSON.generate(message.value).strip}\n\n"
         rescue => e
